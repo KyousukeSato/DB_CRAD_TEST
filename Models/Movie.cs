107141e24db44ab015067721b0e8ae865c33354c
Administrator <Administrator@WIN-O7SMPM3SKUR>
2020-08-01 09:44:17 +0900

    1st comiit

---
+++ Models/Movie.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+       
+    }
+}
